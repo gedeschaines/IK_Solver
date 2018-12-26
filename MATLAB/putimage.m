77ed6726cc992aa6db281eb6c29a7505e921a84c
Gary Deschaines <gedeschaines@gmail.com>
2018-12-25 23:51:48 -0600

    First attempt at forcing image size to match MATLAB getframe().

--- MATLAB/putimage.m
+++ MATLAB/putimage.m
@@ -1,8 +1,15 @@
-function [ fpath ] = putimage(h, i, fmt)
+function [ fpath ] = putimage(h, i, fmt, p3D)
 %% Saves image associated with given figure handle 'h'and frame index
-%% 'i' in format specified by 'fmt'. Returns path to saved image file.
+%% 'i' in format specified by 'fmt' and size according to p3D. Returns
+%% path to saved image file.
 
   fpath = sprintf ("./images/%ss/img_%04d.%s", fmt, i, fmt);
-  saveas(h, fpath, fmt);
+  pdev = sprintf('"-d%s"', fmt);
+  if p3D == 0
+    psiz = "-S570,450";
+  else
+    psiz = "-S560,420";
+  end
+  print(h, fpath, psiz)
   
 endfunction
