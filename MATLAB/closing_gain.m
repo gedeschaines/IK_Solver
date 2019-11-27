5e3e3a9828b431086537081ccb2e311ec55475f9
Gary Deschaines <gedeschaines@gmail.com>
2019-11-26 22:13:40 -0600

    Changed '|' to '||' in first if expression.

--- MATLAB/closing_gain.m
+++ MATLAB/closing_gain.m
@@ -7,27 +7,27 @@ function [gain] = closing_gain(ikm,ec,pt,vt)
 %   pt  = position of target in world space
 %   vt  = velocity of target in world space
 
-  de  = ec - pt;
-  nde = norm(de);
-  nvt = norm(vt);
-  if (nde == 0.0) | (nvt == 0.0)
-    gain = 1.0;
+de  = ec - pt;
+nde = norm(de);
+nvt = norm(vt);
+if (nde == 0.0) || (nvt == 0.0)
+  gain = 1.0;
+else
+  % compute cosine of approach angle relative to target velocity vector
+  aacos = dot(de/nde,vt/nvt);
+  if aacos <= 0.0
+    gain = 1.6;
   else
-    % compute cosine of approach angle relative to target velocity vector
-    aacos = dot(de/nde,vt/nvt);
-    if aacos <= 0.0
-      gain = 1.6;
+    if aacos <= 0.7071
+      gain = 1.2;
     else
-      if aacos <= 0.7071
-        gain = 1.2;
+      if ikm < 6
+        gain = 1.0;  % without null space control
       else
-        if ikm < 6
-          gain = 1.0;  % without null space control
-        else
-          gain = 0.8;  % with null space control
-        end
+        gain = 0.8;  % with null space control
       end
     end
   end
+end
 
 end
