693a570e91ee25992e43812c87e6a7936232e945
Gary Deschaines <gedeschaines@gmail.com>
2019-11-26 22:12:00 -0600

    Removed extraneous trailing right paren in fprintf statements.

--- MATLAB/angle_chk.m
+++ MATLAB/angle_chk.m
@@ -15,15 +15,15 @@ a = q(k)*dpr;
 fprintf('joint %1d rotation angle from inertial x-axis  = %12.6f\n',k,a);
 for k = 2:n 
   a = q(k)*dpr;
-  fprintf('joint %1d rotation angle from joint %1d x-axis   = %12.6f\n',k,k-1,a));
+  fprintf('joint %1d rotation angle from joint %1d x-axis   = %12.6f\n',k,k-1,a);
 end
 asum = sum(q)*dpr;
-fprintf('angle to joint %1d x-axis from inertial x-axis = %12.6f\n',n,asum));
+fprintf('angle to joint %1d x-axis from inertial x-axis = %12.6f\n',n,asum);
 etp = et - p{n};  % vector from joint n to target
 det = norm(etp);  % distance from joint n to target
 at3 = acos((etp*(rotation(sum(q),u{n})*[1;0;0]))/det)*dpr;
 atI = acos((etp*[1;0;0])/det)*dpr;
-fprintf('angle to target from joint %1d x-axis          = %12.6f\n',n,at3));
-fprintf('angle to target from inertial x-axis         = %12.6f\n',atI));
+fprintf('angle to target from joint %1d x-axis          = %12.6f\n',n,at3);
+fprintf('angle to target from inertial x-axis         = %12.6f\n',atI);
 
 end
