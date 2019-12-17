a59a73e6379d37ef7f29dbc35283df0863822094
Gary Deschaines <gedeschaines@gmail.com>
2019-12-17 15:55:43 -0600

    Changes for BGE compatibility with UPBGE v0.2.4b.

--- Blender/FourLink3D_BGE/scripts/moveEmpty.py
+++ Blender/FourLink3D_BGE/scripts/moveEmpty.py
@@ -1,7 +1,8 @@
-import bge
-
-# Use with Blender Game Engine
-# ============================
+# FILE: moveEmpty.py
+# PROG: fourlink3d_BGE.blend
+# DESC: Blender Game Engine logic block controller script for
+#       keyboard control of Empty frame motion utilized as the
+#       FourLink3D robotic manipulator arm end effector IK target.
 # 
 # NOTE: Select layer 1 (solids) and layer 2 (armature)
 #       in 3D View before pressing 'P' key to initiate
@@ -17,39 +18,56 @@ import bge
 #                 TargetPath curve in +Y direction.
 #   space bar   - position target at start of the
 #                 TargetPath curve with zero velocity.
-           
+
+import bge
+from mathutils import Vector
+
+# Acquire current controller and its owner object.
 cont = bge.logic.getCurrentController()
 obj  = cont.owner
 
+# Acquire controller's keyboard keypress sensors.
 pressL = cont.sensors["left"]
 pressR = cont.sensors["right"]
 pressS = cont.sensors["space"]
 
+# Acquire controller's actuator and its linear velocity.
 move = cont.actuators["move"]
-velX = move.dLoc[0]
-velY = move.dLoc[1]
-velZ = move.dLoc[2]
-    
+velX = move.linV[0]
+velY = move.linV[1]
+velZ = move.linV[2]
+
+# Based on which keypress sensor exhibits a positive signal:
+#
+# 1) adjust (decr., incr. or zero) actuator linear velocity
+# 2) force immediate update of owner object linear velocity
+# 3) activate or deactivate the actuator
+
 if pressL.positive:
+    # decrement world space linear velocity
+    velX = velX - 0.0
+    velY = velY - 0.2
+    velZ = velZ - 0.0
+    move.linV = Vector((velX, velY, velZ))
+    obj.setLinearVelocity(move.linV, False)
     cont.activate(move)
-    velX = velX - 0.00
-    velY = velY - 0.05
-    velZ = velZ - 0.00
-    move.dLoc = [velX, velY, velZ]
 
 elif pressR.positive:
-    cont.activate(move)
-    velX = velX + 0.00
-    velY = velY + 0.05
-    velZ = velZ + 0.00
-    move.dLoc = [velX, velY, velZ]
+    # increment world space linear velocity
+    velX = velX + 0.0
+    velY = velY + 0.2
+    velZ = velZ + 0.0
+    move.linV = Vector((velX, velY, velZ))
+    obj.setLinearVelocity(move.linV, False)
     cont.activate(move)
     
 elif pressS.positive:
+    # reset world space linear velocity and position
     velX = 0.0
     velY = 0.0
     velZ = 0.0
-    move.dLoc = [velX, velY, velZ]
-    obj.worldPosition = [3.0, 4.0, 2.0]
+    move.linV = Vector((velX, velY, velZ))
+    obj.setLinearVelocity(move.linV, False)
+    obj.worldPosition = Vector((3.0, 4.0, 2.0))
     cont.deactivate(move)
     
